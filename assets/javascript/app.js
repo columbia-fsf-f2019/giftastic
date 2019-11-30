3dc28f0c567e03369e5b5e1cb2d31d42b50494e5
Ben Florentino <florentinob92@gmail.com>
2019-11-30 11:24:23 -0500

    HTML Code Created / Initial Work On JAVA

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    var topics = ["Pokemon", "Dragonball Z", "Family Guy", "Samurai Jack", "Beavis and Butt-Head", "Scooby-Doo", "Johnny Bravo", "Adventure Time", "South Park"];
+
+    function displayInfo() {
+        var cartoon = $(this).attr("cartoon-name");
+        var queryURL = ""
+
+
+    }
+});
