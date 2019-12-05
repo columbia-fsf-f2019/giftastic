96371262d56f9af8844a7247958f19c2bc56cd6d
Paul Ballas <paul.ballas@richmond.edu>
2019-12-05 14:11:36 -0500

    build significant functionality into page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,14 +18,15 @@ var buttonInput;
 var buttonResponseArr;
 var buttonURL;
 var searchInput;
-var searchResponseArr;
-var searchURL;
 
-for (var i = 0; i < buttonArray.length; i++) {
-  var topButtons = $("<button>").text(buttonArray[i]);
-  topButtons.addClass("topButton btn btn-primary m-2");
-  $(".button-holder").append(topButtons);
+function renderButtons() {
+  for (var i = 0; i < buttonArray.length; i++) {
+    var topButtons = $("<button>").text(buttonArray[i]);
+    topButtons.addClass("topButton btn btn-primary m-2");
+    $(".button-holder").append(topButtons);
+  }
 }
+renderButtons();
 
 $(".button-holder").on("click", ".topButton", function() {
   $(".row1").html("");
@@ -49,11 +50,10 @@ $(".search-btn").click(function(event) {
   searchInput = $("#gifInput")
     .val()
     .trim();
-  searchURL = `http://api.giphy.com/v1/gifs/search?q=${searchInput}&api_key=1HmF2ApULJrVnZjcuLpVIaVwjx8w6ZCi&limit=${limit}`;
-  $.ajax(searchURL).then(function(response) {
-    searchResponseArr = response.data;
-    renderGifs(searchResponseArr);
-  });
+
+  buttonArray.push(searchInput);
+  $(".button-holder").html("");
+  renderButtons();
 });
 
 function renderGifs(array) {
@@ -61,16 +61,35 @@ function renderGifs(array) {
     if (i < 3) {
       $(
         ".row1"
-      )[0].innerHTML += `<div class="col-4"><p>${array[i].title}<img src=${array[i].url}></p></div>`;
+      )[0].innerHTML += `<div class="col-4"><p mb-5>${array[i].title}</p><img
+      src="${array[i].images.fixed_height_small.url}"
+      data-state="animated"
+      class="giphy-embed"
+      allowfullscreen
+    ></div>`;
     }
     if (i > 3 && i < 7) {
       $(
         ".row2"
-      )[0].innerHTML += `<div class="col-4"><p>${array[i].title}<img src=${array[i].url}></p></div>`;
+      )[0].innerHTML += `<div class="col-4"><p mb-5>${array[i].title}</p><img
+      src="${array[i].images.fixed_height_small.url}"
+      data-state="animated"
+      class="giphy-embed"
+      allowfullscreen
+    ></div>`;
     } else if (i > 7) {
       $(
         ".row3"
-      )[0].innerHTML += `<div class="col-4"><p>${array[i].title}<img src=${array[i].url}></p></div>`;
+      )[0].innerHTML += `<div class="col-4"><p mb-5>${array[i].title}</p><img
+      src="${array[i].images.fixed_height_small.url}"
+      data-state="animated"
+      class="giphy-embed"
+      allowfullscreen
+    ></div>`;
     }
   }
 }
+
+$(".gifContainer").on("click", ".giphy-embed", function() {
+  console.log("works");
+});
