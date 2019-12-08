ef35de85d3563994504093eb6f9efec013a5a961
Edward Harley <edharley19@gmail.com>
2019-12-07 19:36:28 -0500

    feat: Added initial buttons

--- app.js
+++ app.js
@@ -1,38 +1,68 @@
 $(()=> {
 
 
+var animeBtn;
+var anime;
+var animes = ["Naruto", "DragonBall", "Kimi No Na Wa", "One Piece", "Cowboy Bebop"];
 
+var initialBtns = animes.map(value => {
+   return $("<button>").addClass("anime").text(value).click((e)=> {
+        anime = e.target.innerHTML;
+        onBtnClick();
+        
+    });
+    
+});
 
+$("#btnsHere").append(initialBtns);
 
-
-$("button").on("click", (e) => {
-    e.preventDefault();
-    var anime = $("input").val().trim();
+function onBtnClick() {
+    
+    
     var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + anime + "&api_key=cIHSqwOmX0ajlsyyyBBeIyJLB35AdXTP&limit=5";
-    $.ajax( {
+    $.ajax({
         url: queryUrl,
         method: "GET"
     }).then((response)=> {
         console.log(response);
         var results = response.data;
         for (i = 0; i < results.length; i++) {
-          // Creating and storing a div tag
+          
           var animeDiv = $("<div>");
+          
 
-          // Creating a paragraph tag with the result item's rating
           var p = $("<p>").text("Rating: " + results[i].rating);
 
-          // Creating and storing an image tag
           var animeImage = $("<img>");
-          // Setting the src attribute of the image to a property pulled off the result item
+
+          
+          
           animeImage.attr("src", results[i].images.fixed_height.url);
           animeDiv.append(p);
           animeDiv.append(animeImage);
           $("#gifsHere").prepend(animeDiv);
+          
         }
+    });    
+
+}
+
+$("#submitBtn").on("click", (e) => {
+    e.preventDefault();
+    anime = $("input").val().trim();
+    onBtnClick(); 
+    animeBtn = $("<button>").addClass("anime").text(anime).click((e)=> {
+        anime = e.target.innerHTML;
+        onBtnClick();
     });
+    $("#btnsHere").prepend(animeBtn);
+   
 });
 
 
+
 });
+
+
+
 
