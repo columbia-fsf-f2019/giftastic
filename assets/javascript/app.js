fb3a021a1a78d08fff16dd6b2355f18e9df836eb
Pamela Vasquez <pamelavasquez94@gmail.com>
2019-12-08 14:22:56 -0500

    DOC: JS for user to add new mood added.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,55 @@
 //create an array of moods
 var moods = ["happy", "sad", "excited", "chill", "worried", "scared"];
-//create a button element for each array item
-moods.forEach(mood => {
-  $("#moodbtns").append(
-    `<button class="btn btn-info" id="${mood}">${mood}</button>`
-  );
-  //add event listner to each mood button
-  $(`#${mood}`).click(function() {
-    //on button click search gifphy api for the mood
-    var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=aNwhcv4oH9jBS3tvywdTqyJ5KTnpHVuS&q=${mood}&limit=10&offset=0&rating=G&lang=en`;
-    $.ajax(queryURL).then(function(response) {
-      clearGifs();
-      //return 10 gifs on the DOM for the selected mood
-      var gifNumber;
-      for (i = 0; i < response.data.length; i++) {
-        gifNumber = i;
-        getNewGif();
-      }
-      function getNewGif() {
-        var gifURL = response.data[gifNumber].images.fixed_height.url;
-        var imageTag = $("<img>");
-        imageTag.attr("src", gifURL);
-        imageTag.attr("alt", `${mood} giphy`);
-        $(".col-sm-7").append(imageTag);
-      }
-      //clear the gif field everytime a new mod is clicked
-      function clearGifs() {
-        $(".col-sm-7").html("");
-      }
-    });
-  });
-});
 var newmood;
 $("#newmood").click(function() {
   newmood = $("input")
     .val()
     .trim();
   moods.push(newmood);
-  $("#moodbtns").append(
-    `<button class="btn btn-info" id="${newmood}">${newmood}</button>`
-  );
+  clearGifBtns();
+  giftastic();
 });
-//listen for
+function giftastic() {
+  moods.forEach(mood => {
+    //create a button element for each array item
+    $("#moodbtns").append(
+      `<button class="btn btn-info" id="${mood}">${mood}</button>`
+    );
+    //add event listner to each mood button
+    $(`#${mood}`).click(function() {
+      //on button click search gifphy api for the mood
+      var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=aNwhcv4oH9jBS3tvywdTqyJ5KTnpHVuS&q=${mood}&limit=10&offset=0&lang=en`;
+      $.ajax(queryURL).then(function(response) {
+        clearGifs();
+        console.log(response);
+        //return 10 gifs on the DOM for the selected mood
+        var gifNumber;
+        for (i = 0; i < response.data.length; i++) {
+          gifNumber = i;
+          getNewGif();
+        }
+        function getNewGif() {
+          var gifURL = response.data[gifNumber].images.fixed_width.url;
+          var rating = response.data[gifNumber].rating;
+          var imageTag = $("<img>");
+          imageTag.attr("src", gifURL);
+          imageTag.attr("alt", `${mood} giphy`);
+          $("#giphys").append(
+            `<div class="col-sm-4"><h3>Rating: ${rating}</h3>${imageTag[0].outerHTML}</div>`
+          );
+        }
+      });
+    });
+  });
+}
+giftastic();
+//listen for new mood input from user and create button for mood
+
+//clear the gif field everytime a new mod is clicked
+function clearGifs() {
+  $("#giphys").html("");
+}
+//clear gif btns
+function clearGifBtns() {
+  $("#moodbtns").html("");
+}
