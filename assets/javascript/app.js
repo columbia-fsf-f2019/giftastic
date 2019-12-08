c78a2e521ec3fed5d534f3fe48bceb96a46b2624
Pamela Vasquez <pamelavasquez94@gmail.com>
2019-12-08 15:41:04 -0500

    DOCS: update js file to not allow blank entries from user and switch gif from still to animated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,28 @@
 //create an array of moods
-var moods = ["happy", "sad", "excited", "chill", "worried", "scared"];
+var moods = [
+  "happy",
+  "sad",
+  "excited",
+  "chill",
+  "worried",
+  "scared",
+  "cheerful",
+  "gloomy",
+  "pensive"
+];
 var newmood;
 //listen for new mood input from user and create button for mood
 $("#newmood").click(function() {
   newmood = $("input")
     .val()
     .trim();
-  moods.push(newmood);
-  clearGifBtns();
-  giftastic();
+  if (newmood !== "") {
+    moods.push(newmood);
+    clearGifBtns();
+    giftastic();
+  } else {
+    alert("Invalid Entry");
+  }
 });
 
 function giftastic() {
@@ -31,15 +45,32 @@ function giftastic() {
           getNewGif();
         }
         function getNewGif() {
+          var stillGifURL =
+            response.data[gifNumber].images.fixed_width_still.url;
           var gifURL = response.data[gifNumber].images.fixed_width.url;
           var rating = response.data[gifNumber].rating;
           var imageTag = $("<img>");
-          imageTag.attr("src", gifURL);
+          imageTag.attr("src", stillGifURL);
+          imageTag.attr("data-still", stillGifURL);
+          imageTag.attr("data-animate", gifURL);
+          imageTag.attr("data-state", "still");
+          imageTag.attr("class", "gif");
           imageTag.attr("alt", `${mood} giphy`);
           $("#giphys").append(
             `<div class="col-sm-4"><h3>Rating: ${rating}</h3>${imageTag[0].outerHTML}</div>`
           );
         }
+        //swtich gifs from still to animated
+        $(".gif").click(function() {
+          var state = $(this).attr("data-state");
+          if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+          }
+        });
       });
     });
   });
