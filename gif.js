fe2585d8654257e08e125a86129cfc5ede7605d3
Tuscany <lizlevine.rigie@gmail.com>
2019-12-08 22:04:59 -0500

    refactored js, edited css

--- gif.js
+++ gif.js
@@ -1,5 +1,5 @@
 $(function() {
-  populateButtons(houndArr, "clickButton", "#buttonsArea");
+  buttonZone(houndArr, "clickButton", "#buttons-here");
 });
 
 var houndArr = [
@@ -15,32 +15,29 @@ var houndArr = [
   "American foxhound"
 ];
 
-// function to make buttons and add to page
-function populateButtons(houndArr, classToAdd, areaToAddTo) {
-  $(areaToAddTo).empty();
+function buttonZone(houndArr, classToAdd, addHere) {
+  $(addHere).empty();
   for (var i = 0; i < houndArr.length; i++) {
     var dog = $("<button>");
     dog.addClass(classToAdd);
     dog.attr("data-type", houndArr[i]);
     dog.text(houndArr[i]);
-    $(areaToAddTo).append(dog);
+    $(addHere).append(dog);
   }
 }
 
-//
-
 $(document).on("click", ".clickButton", function() {
   var type = $(this).data("type");
+  console.log(type);
   var queryURL =
     "http://api.giphy.com/v1/gifs/search?q=" +
     type +
     "&api_key=Fgt66xFtfUxFToAkOR3jZQ59AxAMJXzl&limit=10";
 
-  // "http://api.giphy.com/v1/gifs/random?api_key=Fgt66xFtfUxFToAkOR3jZQ59AxAMJXzl&tag=";
   $.ajax({ url: queryURL, method: "GET" }).done(function(response) {
-    // console.log(response);
+    console.log(response);
     for (var i = 0; i < response.data.length; i++) {
-      var searchDiv = $('<div class="search-item">');
+      var gifDiv = $('<div class="search-item">');
       var rating = response.data[i].rating;
       var p = $("<p>").text("Rating: " + rating);
       var animated = response.data[i].images.fixed_height.url;
@@ -52,18 +49,18 @@ $(document).on("click", ".clickButton", function() {
       image.attr("data-still", still);
       image.attr("data-animated", animated);
       image.attr("data-state", "still");
-      image.addClass("searchImage");
+      image.addClass("image");
       // referrence paragraph tag with rating
-      searchDiv.append(p);
-      searchDiv.append(image);
-      $("#searches").append(searchDiv);
+      gifDiv.append(p);
+      gifDiv.append(image);
+      $("#search-field").append(gifDiv);
     }
   });
 });
 
-// write some function that will make images go from still to animate
+// write some function that will make images go from still to animate to still upon click
 
-$(document).on("click", ".searchImage", function() {
+$(document).on("click", ".image", function() {
   var state = $(this).attr("data-state");
   if (state === "still") {
     $(this).attr("src", $(this).data("animated"));
@@ -74,12 +71,18 @@ $(document).on("click", ".searchImage", function() {
   }
 });
 
+// SUBMIT BUTTON BUG! tried several fixes, clears page of images, won't load new buttons
+// type in field and click in field will render new buttons
 $("#search-input").on("click", function() {
-  // .eq grabs 1st input in html (not submit)
+  // .eq grabs 1st input in html w/ text (not submit)
   var newFind = $("input")
     .eq(0)
     .val();
+
   houndArr.push(newFind);
-  populateButtons(houndArr, "clickButton", "#buttonsArea");
-  return false;
+
+  buttonZone(houndArr, "clickButton", "#buttons-here");
+
+  buttonZone(houndArr, "clickButton", "#buttons-here");
+  return true;
 });
